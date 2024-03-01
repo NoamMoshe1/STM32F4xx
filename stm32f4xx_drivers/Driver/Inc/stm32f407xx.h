8a616a95fd2b98afee208cfac782ed10f7e96a6e
NOAM MOSHE <non55555@gmail.com>
2024-03-01 19:28:38 +0200

    Finish writing code for 005button_interrupt
    Activating an interrupt when a button is pressed and as a result the LED lights up.
    The button and the LED are connected externally to pin5 and pin12 in the GPIOD.
    I also used resistors to limit the current.

--- stm32f4xx_drivers/Driver/Inc/stm32f407xx.h
+++ stm32f4xx_drivers/Driver/Inc/stm32f407xx.h
@@ -13,6 +13,38 @@
 
 #define __vo volatile
 
+/**********************************START:Processor Specific Details **********************************/
+/*
+ * ARM Cortex Mx Processor NVIC ISERx register Addresses
+ */
+
+#define NVIC_ISER0          ( (__vo uint32_t*)0xE000E100 )
+#define NVIC_ISER1          ( (__vo uint32_t*)0xE000E104 )
+#define NVIC_ISER2          ( (__vo uint32_t*)0xE000E108 )
+#define NVIC_ISER3          ( (__vo uint32_t*)0xE000E10c )
+
+/*
+ * ARM Cortex Mx Processor NVIC ICERx register Addresses
+ */
+
+#define NVIC_ICER0 			( (__vo uint32_t*)0XE000E180 )
+#define NVIC_ICER1			( (__vo uint32_t*)0XE000E184 )
+#define NVIC_ICER2  		( (__vo uint32_t*)0XE000E188 )
+#define NVIC_ICER3			( (__vo uint32_t*)0XE000E18C )
+
+
+/*
+ * ARM Cortex Mx Processor Priority Register Address Calculation
+ */
+
+#define NVIC_PR_BASE_ADDR 	( (__vo uint32_t*)0xE000E400 )
+
+/*
+ * ARM Cortex Mx Processor number of priority bits implemented in Priority Register
+ */
+
+#define NO_PR_BITS_IMPLEMENTED  4
+
 /*
  * Base addresses of Flash and SRAM memories
  * FROM 2.Memory and bus architecture
@@ -468,8 +500,22 @@ typedef struct
  * macros for all the possible priority levels
  */
 
-#define NVIC_IRQ_PRI0    0
-#define NVIC_IRQ_PRI15    15
+#define NVIC_IRQ_PRI0      0
+#define NVIC_IRQ_PRI1      1
+#define NVIC_IRQ_PRI2      2
+#define NVIC_IRQ_PRI3      3
+#define NVIC_IRQ_PRI4      4
+#define NVIC_IRQ_PRI5      5
+#define NVIC_IRQ_PRI6      6
+#define NVIC_IRQ_PRI7      7
+#define NVIC_IRQ_PRI8      8
+#define NVIC_IRQ_PRI9      9
+#define NVIC_IRQ_PRI10     10
+#define NVIC_IRQ_PRI11     11
+#define NVIC_IRQ_PRI12     12
+#define NVIC_IRQ_PRI13     13
+#define NVIC_IRQ_PRI14     14
+#define NVIC_IRQ_PRI15     15
 
 //some generic macros
 
--- stm32f4xx_drivers/Driver/Src/stm32f407xx_gpio_driver.c
+++ stm32f4xx_drivers/Driver/Src/stm32f407xx_gpio_driver.c
@@ -103,27 +103,27 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 		}else if (pGPIOHandle -> GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_RT)
 		{
 			//1 . configure the RTSR
-			EXTI->RTSR |= ( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
+			EXTI -> RTSR |= ( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
 			//Clear the corresponding RTSR bit
-			EXTI->FTSR &= ~( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
+			EXTI -> FTSR &= ~( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
 
 		}else if (pGPIOHandle -> GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_RFT)
 		{
 			//1. configure both FTSR and RTSR
-			EXTI->RTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+			EXTI -> RTSR |= ( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
 			//Clear the corresponding RTSR bit
-			EXTI->FTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+			EXTI -> FTSR |= ( 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber);
 		}
 
 		//2. configure the GPIO port selection in SYSCFG_EXTICR
-		uint8_t temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 4 ;
-		uint8_t temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 4;
-		uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+		uint8_t temp1 = pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber / 4 ;
+		uint8_t temp2 = pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber % 4;
+		uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle -> pGPIOx);
 		SYSCFG_PCLK_EN();
 		SYSCFG -> EXTICR[temp1] = portcode << ( temp2 * 4);
 
 		//3 . enable the exti interrupt delivery using IMR
-		EXTI->IMR |= 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
+		EXTI -> IMR |= 1 << pGPIOHandle -> GPIO_PinConfig.GPIO_PinNumber;
 	}
 
 	//2. configure the speed
@@ -241,10 +241,64 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
  */
 void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
 {
+	if (EnorDi == ENABLE)
+		{
+			if (IRQNumber <= 31)
+			{
+				//program ISER0 register
+				*NVIC_ISER0 |= ( 1 << IRQNumber );
+
+			}
+			else if (IRQNumber > 31 && IRQNumber < 64 ) //32 to 63
+			{
+				//program ISER1 register
+				*NVIC_ISER1 |= ( 1 << (IRQNumber % 32) );
+			}
+			else if (IRQNumber >= 64 && IRQNumber < 96 )
+			{
+				//program ISER2 register //64 to 95
+				*NVIC_ISER2 |= ( 1 << (IRQNumber % 64) );
+			}
+		}
+	else
+		{
+			if (IRQNumber <= 31)
+			{
+				//program ICER0 register
+				*NVIC_ICER0 |= ( 1 << IRQNumber );
+			}
+			else if (IRQNumber > 31 && IRQNumber < 64 )
+			{
+				//program ICER1 register
+				*NVIC_ICER1 |= ( 1 << (IRQNumber % 32) );
+			}
+			else if (IRQNumber >= 64 && IRQNumber < 96 )
+			{
+				//program ICER2 register
+				*NVIC_ICER2 |= ( 1 << (IRQNumber % 64) );
+			}
+		}
 
 }
 
-void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
-void GPIO_IRQHandling(uint8_t PinNumber);
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
+{
+	//1. first lets find out the ipr register
+	uint8_t iprx = IRQNumber / 4;
+	uint8_t iprx_section  = IRQNumber %  4;
 
+	uint8_t shift_amount = ( 8 * iprx_section) + ( 8 - NO_PR_BITS_IMPLEMENTED) ;
+
+	*( NVIC_PR_BASE_ADDR + iprx ) |=  ( IRQPriority << shift_amount );
+
+}
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+	//clear the exti pr register corresponding to the pin number
+	if (EXTI -> PR & ( 1 << PinNumber))
+	{
+		//clear
+		EXTI -> PR |= ( 1 << PinNumber);
+	}
+}
 
