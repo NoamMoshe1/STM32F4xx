ac300636cad7e3da9c85cd5bb4a81112b29c87ae
NOAM MOSHE <non55555@gmail.com>
2024-03-22 10:16:32 +0200

    Finished writing code for SPI CMD handling (008)
    Finished writing code for SPI tx_rx Success sending and receiving data to arduino (008008spi_cmd_handling.c)
    
    * Pressing the B1 button activates sending an SPI message to the Arduino and returning a response from the Arduino via SPI.
    * Since the voltage data of the Arduino is different from the STM32 board, I used a logic level converter module, a bi-directional module for Arduino boost 3V to 5V.
    
    The following commands were executed:
    * CMD_LED_CTRL
    * COMMAND_SENSOR_READ
    * COMMAND_LED_READ
    * COMMAND_PRINT
    * COMMAND_ID_READ
    
    * I wrote a function for SPI Rx.
    * In addition, the Arduino side is written.

--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,19 +5,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/005button_interrupt.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../Src/008spi_cmd_handling.c \
+../Src/syscalls.c 
 
 OBJS += \
-./Src/005button_interrupt.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/008spi_cmd_handling.o \
+./Src/syscalls.o 
 
 C_DEPS += \
-./Src/005button_interrupt.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./Src/008spi_cmd_handling.d \
+./Src/syscalls.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -27,7 +24,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/005button_interrupt.cyclo ./Src/005button_interrupt.d ./Src/005button_interrupt.o ./Src/005button_interrupt.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/008spi_cmd_handling.cyclo ./Src/008spi_cmd_handling.d ./Src/008spi_cmd_handling.o ./Src/008spi_cmd_handling.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su
 
 .PHONY: clean-Src
 
